39cb84016cba5fa90eb07224006ea23d241a9992
Stas Usenko <Stasusenko@gmail.com>
2020-09-15 15:54:50 +0300

    many new files

--- TeleBot.py
+++ TeleBot.py
@@ -1,18 +1,19 @@
 import telebot
 import pickle
 
-
 bot = telebot.TeleBot('1012392887:AAHoiinJ809WkYfhC3gMzsBmEXcS2Lrypss')
 
 categories = [['food', 'еда', "продукты", "жратва", 'хлеб', 'атб', 'варус', 'сильпо', 'магазин'],
               ['alcohol', 'пиво', 'вино', 'бухло', 'пивас', 'алкоголь', 'выпивка', 'сидр', 'ром'],
               ['coffee', 'кофе', 'чай'],
-              ['public services', 'комуналка', 'свет', 'вода', 'газ', 'отопление', 'коммунальные услуги', 'жилье', 'газ'],
+              ['public services', 'комуналка', 'свет', 'вода', 'газ', 'отопление', 'коммунальные услуги', 'жилье',
+               'газ'],
               ['transport', 'трамвай', 'проезд', 'дорога', 'маршрутка'],
               ['credit', 'кредит', 'долг'],
               ['trip', 'путешествие', 'поездка', 'отпуск'],
               ['other', 'другое', 'еще'],
               ]
+
 def save_budget(content):
     with open('budget.txt', 'wb') as file:
         pickle.dump(content, file)
@@ -25,9 +26,11 @@ def load_budget():
     except:
         pass
 
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Привет')
+
 
 @bot.message_handler(commands=['current'])
 def current_message(message):
@@ -39,6 +42,7 @@ def current_message(message):
     reply = ''.join(current)
     bot.send_message(message.chat.id, reply)
 
+
 @bot.message_handler(commands=['categories'])
 def categories_message(message):
     budget = load_budget()
@@ -47,6 +51,7 @@ def categories_message(message):
         categories.append(key + '\n')
     reply = ''.join(categories)
     bot.send_message(message.chat.id, reply)
+
 
 def parse_added_message(message):
     what = message.text.split(' ')
@@ -60,10 +65,11 @@ def parse_added_message(message):
                 str = k.lower()
         return value, str
 
+
 @bot.message_handler(content_types=['text'])
 def added(message):
+    counter_to_error = 0
     budget = load_budget()
-    Fail = False
     try:
         value, str = parse_added_message(message)
     except:
@@ -72,22 +78,23 @@ def added(message):
     for cat in categories:
         if str in cat:
             budget['{}'.format(cat[0])] += value
+            print(budget)
             if value > 0:
-                bot.send_message(message.chat.id, 'Окей, {} грн потрачено на {}'.format(value, str))
-                return 0
-            else:
-                bot.send_message(message.chat.id, 'Окей, {} грн вычли из {}'.format(value, str))
-                return 0
-    bot.send_message(message.chat.id, 'Не найдено похожей категории. Попробуй еще раз.')
-    save_budget(budget)
+                bot.send_message(message.chat.id, 'Окей, {} грн --> {}'.format(value, str))
+            elif value <= 0:
+                bot.send_message(message.chat.id, 'Окей, {} грн <-- {}'.format(value * -1, str))
+        else:
+            counter_to_error += 1
+    if counter_to_error == 8:
+        bot.send_message(message.chat.id, "Такой категории не существует. Список доступных категорий /categories")
+        return 0
+    else:
+        save_budget(budget)
+
 
 @bot.message_handler(commands=['previos'])
 def previos(message):
     bot.send_message(message.chat.id, 'Нет данных о предидущем месяце')
-
-
-
-
 
 
 bot.polling()
